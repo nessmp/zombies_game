d0b548b6351d00e8007d34e1faa33fa55ada9c00
nessmp <nestormp27@gmail.com>
2020-06-09 01:46:53 -0500

    added credits scene

---
+++ zombies/Assets/Scripts/Buttons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+    public void ChangeSceneButton(string scene){
+        if (scene == "Quit") {
+            Application.Quit();
+        } else {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
